30ec27900cf53c418892dfec277c30bac56e9eaa
bharadwaj6 <krishna.bharadwaj6@gmail.com>
2014-10-05 20:07:51 +0530

    removed the patterns cases, as we don't need to print patterns, and wrote simple recursive implementation

--- solution.py
+++ solution.py
@@ -16,82 +16,87 @@ from positions f+1 => t and f => t+1.
 This solution assumes we can print f and t in such cases as an alternate path.
 """
 
-
-def pre_pattern(positions, n, moves):
-	positions[2*n-2], positions[2*n-1] = positions[4*n-3], positions[4*n-2]
-	positions[4*n-3], positions[4*n-2] = '', ''
-	moves.append((4*n-3, 2*n-2))
-	positions[4*n-3], positions[4*n-2] = positions[2*n+2], positions[2*n+3]
-	positions[2*n+2], positions[2*n+3] = '', ''
-	moves.append((2*n+2, 4*n-3))
-	return [positions, moves]
-
-def post_pattern(positions, n, moves):
-	positions[2*n], positions[2*n-1] = positions[4*n-1], positions[4*n-2]
-	positions[4*n-1], positions[4*n-2] = '', ''
-	moves.append((4*n-2, 2*n-1))
-	return [positions, moves]
-
-# n = int(raw_input())
 def baggage_solver(n):
-	""" If n == 3, it solves the baggage problem by brute force.
-	If n > 3, we create a pattern of the form ABBA....BBAA,
+	""" 
+	Reduces the pattern to the form ABBA....BBAA,
 	and then create pairs of A's and B's in the middle until
-	we have all As and Bs in pairs, and then rearrange."""
-	
+	we have all As and Bs in pairs, and then rearrange.
+	"""
+	if n == 3:
+		# special case solution for n == 3, as it goes until index -3.
+		moves = [(2,-1), (5,2), (3,-3)]
+		return moves
 	if n > 7:
 		"""
 		from n == 8, we can reduce pattern to this form: ABBA[__(BA)*]BBAA
 		in which the part inside [] can be solved recursively by calling
 		baggage_solver(n-4) as 4 A's and 4 B's are outside.
 		"""
-		changed_positions = baggage_solver(n-4) # print changed positions with updated index as per n, not n-4
-
-	if n > 3:
+		moves = [(2*n-2, -1), (3, 2*n-2)]
+		recursive_pattern_moves = baggage_solver(n-4)
+		# print changed positions with updated index as per n, not n-4
+		moves += [(x+4,y+4) for (x,y) in recursive_pattern_moves]
+		moves += [(0, 2*n-5), (2*n-1,0)]
+		return moves
+	else:
 		"""
-		returns the positions to be printed for the given (sub)string
+		returns the positions to be printed for the given simple case
 		"""
-		no_pos = 2 * n
-		positions = [''] * no_pos
-		string_list = ['B' if x % 2 == 0 else 'A' for x in xrange(no_pos)]
-		positions += string_list
-		astring = ''.join(string_list)
 		moves = []
 		if n == 4:
-			pp = pre_pattern(positions, n, moves)
-			positions = pp[0]
-			moves = pp[1]
-			# special step for n == 4
-			positions[2*n+2], positions[2*n+3] = positions[2*n], positions[2*n-1]
-			positions[2*n], positions[2*n-1] = '', ''
-			moves.append((2*n, 2*n+2))
-			positions[2*n], positions[2*n-1] = positions[4*n-1], positions[4*n-2]
-			positions[4*n-1], positions[4*n-2] = '', ''
-			moves.append((4*n-1, 2*n-1))
-			for m in moves:
-				print m[0]-2*n+1, " to ", m[1]-2*n+1
+			moves.append((2*n-2, -1))
+			moves.append((n-1, 2*n-2))
+			moves.append((-1, n-1))
+			moves.append((2*n-1,-1))
 		if n == 5:
-			pp = pre_pattern(positions, n, moves)
-			positions = pp[0]
-			moves = pp[1]
-			# special step for n == 5
-			positions[2*n+2], positions[2*n+3] = positions[2*n+5], positions[2*n+4]
-			positions[2*n+4], positions[2*n+5] = '', ''
-			moves.append((2*n+4, 2*n+2))
-			positions[2*n+4], positions[2*n+5] = positions[2*n], positions[2*n-1]
-			positions[2*n], positions[2*n-1] = '', ''
-			moves.append((2*n-1, 2*n+4))
-			pp = post_pattern(positions, n, moves)
-			positions = pp[0]
-			moves = pp[1]
-			for m in moves:
-				print m[0]-2*n+1, " to ", m[1]-2*n+1
+			moves.append((2*n-2, -1))
+			moves.append((n-1, 2*n-2))
+			moves.append((n+1, n-1))
+			moves.append((-1, n+1))
+			moves.append((2*n-1, -1))
+		if n == 6:
+			moves.append((2*n-2, -1))
+			moves.append((2*n-5, 2*n-2))
+			moves.append((2*n-10, 2*n-5))
+			moves.append((n, 2*n-10))
+			moves.append((-1, n))
+			moves.append((2*n-1, -1))
+		if n == 7:
+			moves.append((2*n-2, -1))
+			moves.append((n-2, 2*n-2))
+			moves.append((n+1, n-2))
+			moves.append((n-4, n+1))
+			moves.append((n+2, n-4))
+			moves.append((-1, n+2))
+			moves.append((2*n-1, -1))
+		return moves
 
-	if n == 3:
-		# special case solution for n == 3, as it goes until index -3.
-		pass
 
-print "for n == 4"
-baggage_solver(4)
-print "for  n == 5"
-baggage_solver(5)
+if __name__ == "__main__":
+	print "Enter your input (Enter the character 'x' to run all the cases: "
+	n = raw_input()
+	if n == 'x':
+		for i in xrange(3,101):
+			print "for n == ", i
+			a = baggage_solver(i)
+			for m in a:
+				print m[0], " to ", m[1]
+			print
+		exit()
+
+	try:
+		n = int(n)
+	except ValueError:
+		print "Wrong input, Enter a number or x"
+		exit()
+
+	if not ( 3 <= n <= 100):
+		print "Wrong input, Enter a number between 3 and 100 inclusive"
+	else:
+		for i in xrange(n, n+1):
+			print "for n == ", i
+			a = baggage_solver(i)
+			for m in a:
+				print m[0], " to ", m[1]
+			print 
+
